bc917ee2a4cc902dd4e805d24a1578dc9a8bef53
Fake-Name <something@fake-url.com>
2017-01-06 23:28:48 -0800

    (Untested) multiple node support.

--- dspLog.py
+++ dspLog.py
@@ -7,8 +7,17 @@ import urllib.parse
 import urllib.request
 import random
 
-DSP_OUTLET_IP = '10.1.2.200'
-UPDATE_INTERVAL = 5
+DSP_OUTLETS = [
+		# Add more outlets here. The first number is the node number sent to emoncms,
+		# the second value is the IP address for the specified node.
+		(2, '10.1.2.200'),
+]
+
+# The graphite storage engine averages the values if you send several samples
+# in a single logging interval. I have no idea how emoncms's silly PHP reimplementation
+# works (because it's a stupid idea).
+# Anyways, oversampling is better then undersampling.
+UPDATE_INTERVAL_SECONDS = 5
 
 class DSPInterface(object):
 	def __init__(self, plugIp):
@@ -42,36 +51,56 @@ class DSPInterface(object):
 		return retVal
 
 
+def get_key():
+	paths = [
+		"../emoncmsApiKey.conf",
+		"./emoncmsApiKey.conf"
+	]
+
+	for pathn in paths:
+		try:
+			apiKey = open(pathn, "r").read()
+			return apiKey
+		except FileNotFoundError:
+			pass
+
+	raise RuntimeError("No API key file found! This script expects the api key in a text file named `emoncmsApiKey.conf`,"+
+		" in either the current directory or one directory above the current dir!")
+
+
 
 if __name__ == "__main__":
 	print("Starting")
 
 
-	apiKey = open("../emoncmsApiKey.conf", "r").read()
 
 	monBuf = EmonFeeder.EmonFeeder(protocol = 'https://',
 							  domain = '10.1.1.39',
 							  path = '/emoncms',
-							  apikey = apiKey,
+							  apikey = get_key(),
 							  period = 15)
+	outlets = []
+	for nodenum, nodeip in DSP_OUTLETS:
+		outlets.append((nodenum, DSPInterface(nodeip)))
+	print("Plugs opened: %s" % outlets)
 
-	outlet = DSPInterface(DSP_OUTLET_IP)
-	print("Plug opened. Handle = %s" % outlet)
 
-
-	nextRun = time.time() + UPDATE_INTERVAL
+	nextRun = time.time() + UPDATE_INTERVAL_SECONDS
 	while 1:
 		try:
-			powerReading = outlet.getReading()
+			for outletid, outlet in outlets:
+				powerReading = outlet.getReading()
 
-			if powerReading:
-				print("Have data! Ready to send")
-				monBuf.add_data(["2", 0, powerReading])
-				monBuf.send_data()
+				if powerReading:
+					print("Have data! Ready to send")
+
+					# I don't remember what this zero is here for.
+					monBuf.add_data([str(outletid), 0, powerReading])
+					monBuf.send_data()
 		except:
 			raise
 
 		while time.time() < nextRun:
 			time.sleep(1)
-		nextRun += UPDATE_INTERVAL
+		nextRun += UPDATE_INTERVAL_SECONDS
 		print("LOOPIN")
